a63be71c8b85241da72d004a01d77784eb5bccf1
uutbudiarto2012 <hikids.developer@gmail.com>
2025-04-10 21:13:35 +0700

    og home page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -6,6 +6,20 @@ export async function generateMetadata() {
   return {
     title: 'Home - Uut Budiarto',
     description: 'Portofolio profesional Fullstack Developer dengan pengalaman menggunakan Node.js, Next.js, React.js, dan Nuxt.js. Tersedia showcase proyek dan keahlian teknis untuk kebutuhan rekrutmen IT.',
+    openGraph: {
+      title: 'Home - Uut Budiarto',
+      description: 'Portofolio profesional Fullstack Developer dengan pengalaman menggunakan Node.js, Next.js, React.js, dan Nuxt.js. Tersedia showcase proyek dan keahlian teknis untuk kebutuhan rekrutmen IT.',
+      type: 'article',
+      url: `${process.env.SITE_URL}`,
+      images: [
+        {
+          url: `${process.env.SITE_URL}/images/jawaskrip.png`,
+          width: 512,
+          height: 512,
+          alt: 'uut budiarto',
+        },
+      ],
+    },
   };
 }
 export default function HomePage() {
